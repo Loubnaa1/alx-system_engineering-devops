bde126eb18a7ea8c61b33f1f51af94e635221572
Loubnaa1 <loubnamerzak4@gmail.com>
2024-01-04 08:49:57 -0800

    my_task

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Returns information uber TO-DO list and exports the data in for a given employee ID."""
+"""Returns information uber TO-DO list,
+and exports the data in for a given employee ID."""
 import csv
 import requests
 from sys import argv
@@ -8,14 +9,15 @@ if __name__ == "__main__":
     user_id = argv[1]
     base_url = "https://jsonplaceholder.typicode.com/"
     user_response = requests.get(f"{base_url}users/{user_id}")
-    todos_response = requests.get(f"{base_url}todos", params={"userId": user_id})
+    todor = requests.get(f"{base_url}todos", params={"userId": user_id})
 
     user = user_response.json()
-    todos = todos_response.json()
+    todos = todor.json()
     username = user.get("username")
 
     filename = f"{user_id}.csv"
     with open(filename, mode="w", newline='') as file:
         csv_writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for todo in todos:
-            csv_writer.writerow([user_id, username, todo.get("completed"), todo.get("title")])
+            csv_writer.writerow([user_id, username,
+                todo.get("completed"), todo.get("title")])
