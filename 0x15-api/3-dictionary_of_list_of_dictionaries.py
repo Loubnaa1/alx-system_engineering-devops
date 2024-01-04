c42b2e175e58292be7580d89fa384c7482f1a38b
Loubnaa1 <loubnamerzak4@gmail.com>
2024-01-04 09:11:31 -0800

    my_task

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Python script to export data in the JSON format."""
+import json
+import requests
+
+if __name__ == "__main__":
+    base_url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(f"{base_url}users").json()
+
+    all_users_todo_data = {}
+    for user in users:
+        user_id = user["id"]
+        todos = requests.get(f"{base_url}todos",
+                params={"userId": user_id}).json()
+
+        all_users_todo_data[user_id] = [
+            {
+                "task": todo["title"],
+                "completed": todo["completed"],
+                "username": user["username"]
+            }
+            for todo in todos
+        ]
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(all_users_todo_data, jsonfile)
