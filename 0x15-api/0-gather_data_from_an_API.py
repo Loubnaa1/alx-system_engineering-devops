532093f95c9460b3d83a38c20f8a854414a361c9
Loubnaa1 <loubnamerzak4@gmail.com>
2024-01-04 07:54:43 -0800

    tasks

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Returns inforamation about to-do list for a given employee ID."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    completed_titles = list(map(lambda t: t.get("title"), filter(lambda t: t.get("completed"), todos)))
+
+employee_name = user.get("name")
+total_tasks = len(todos)
+completed_tasks = len(completed_titles)
+print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
+
+for title in completed_titles:
+    print(f"\t {title}")
