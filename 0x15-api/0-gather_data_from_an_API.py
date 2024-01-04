97ddd160a7b856863aa3bab4d0d7df4c1944b4ee
Loubnaa1 <loubnamerzak4@gmail.com>
2024-01-04 08:13:49 -0800

    tasksn

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-"""Returns inforamation to-do list for a given employee ID."""
+"""Returns info uber to-do list, for a given employee ID"""
+
 import requests
 import sys
 
+def main():
+    employee_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/"
+
+    user_response = requests.get(f"{base_url}users/{employee_id}")
+    todos_response = requests.get(f"{base_url}todos", params={"userId": employee_id})
+
+    user = user_response.json()
+    todos = todos_response.json()
+
+    completed_tasks = [todo['title'] for todo in todos if todo.get('completed')]
+
+    user_name = user.get('name', 'Unknown User')
+    print(f"Employee {user_name} is done with tasks({len(completed_tasks)}/{len(todos)}):")
+    for task_title in completed_tasks:
+        print(f"\t {task_title}")
+
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-    completed_titles = list(map(lambda t: t.get("title"), filter(lambda t: t.get("completed"), todos)))
-
-employee_name = user.get("name")
-total_tasks = len(todos)
-completed_tasks = len(completed_titles)
-print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
-
-for title in completed_titles:
-    print(f"\t {title}")
+    main()
