614fbf7f5e197313863c7f91c96fc84f54983ff0
Loubnaa1 <loubnamerzak4@gmail.com>
2024-01-04 08:29:34 -0800

    my_task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,17 +5,17 @@ import requests
 import sys
 
 def main():
-    employee_id = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com/"
-    user_response = requests.get(f"{base_url}users/{employee_id}")
-    todos_response = requests.get(f"{base_url}todos", params={"userId": employee_id})
-    user = user_response.json()
-    todos = todos_response.json()
-    completed_tasks = [todo['title'] for todo in todos if todo.get('completed')]
 
+    em_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user_response = requests.get(f"{base_url}users/{em_id}")
+    todor = requests.get(f"{base_url}todos", params={"userId": em_id})
+    user = user_response.json()
+    todos = todor.json()
+    tasks = [todo['title'] for todo in todos if todo.get('completed')]
     user_name = user.get('name', 'Unknown User')
-    print(f"Employee {user_name} is done with tasks({len(completed_tasks)}/{len(todos)}):")
-    for task_title in completed_tasks:
+    print(f"Employee {user_name} is done with tasks({len(tasks)}/{len(todos)}):")
+    for task_title in tasks:
         print(f"\t {task_title}")
 if __name__ == "__main__":
     main()
