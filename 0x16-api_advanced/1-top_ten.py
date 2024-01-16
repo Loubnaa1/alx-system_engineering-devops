d85c5a6fe1344234c24d2850cb4226b3307ccc35
Loubnaa1 <loubnamerzak4@gmail.com>
2024-01-16 15:41:05 -0800

    my_project

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" contains top_ten function """
+
+import requests
+
+
+def top_ten(subreddit):
+    """function that Displays the titles of the first 10 hot posts listed
+    for a given subreddit"""
+
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "RedditTestApi/1.0"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    for item in results.get("children", []):
+        print(item.get("data", {}).get("title"))
