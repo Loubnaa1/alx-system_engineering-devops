89520eb4f6384c128cd1329f529997bdec1ad54d
Loubnaa1 <loubnamerzak4@gmail.com>
2024-01-16 15:48:31 -0800

    y_p

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,10 +5,9 @@ import requests
 def number_of_subscribers(subreddit):
     """function that returns the number of subscriber"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "RedditApiTest/1.0"}
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
     return results.get("subscribers")
-
