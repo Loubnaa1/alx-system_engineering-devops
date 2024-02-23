e1b1a9780fb0dd38e3922aeaaba64c29d16e3cd2
Loubnaa1 <loubnamerzak4@gmail.com>
2024-02-23 01:35:50 +0000

    proj

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
 # login with holberton user without any error message
 
 exec { 'increase_nofile_limit':
-  command => "/bin/sed -i '/^holberton\s/ s/soft nofile [0-9]\+/soft nofile 4000/' /etc/security/limits.conf && /bin/sed -i '/^holberton\s/ s/hard nofile [0-9]\+/hard nofile 4000/' /etc/security/limits.conf",
+  command => "sed -i '/^holberton\s/ s/soft nofile [0-9]\+/soft nofile 4000/' /etc/security/limits.conf && sed -i '/^holberton\s/ s/hard nofile [0-9]\+/hard nofile 4000/' /etc/security/limits.conf",
   path    => ['/bin', '/usr/bin'],
 }
