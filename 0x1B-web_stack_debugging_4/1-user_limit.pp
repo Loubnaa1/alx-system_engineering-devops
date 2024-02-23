407d514611437c8a468ce70f25a4bbffe6047306
Loubnaa1 <loubnamerzak4@gmail.com>
2024-02-23 02:27:05 +0000

    proje

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,7 @@
 # login with holberton user without any error message
 
-
-exec { 'change_nofile_limit':
-  command => "sed -i '/^nofile/s/.*/nofile 5000/' /etc/security/limits.conf",
+exec { 'update_nofile_limits':
+  command => "sed -i 's/nofile \\+[0-9][0-9]*/nofile 5000/g' /etc/security/limits.conf",
   path    => ['/bin', '/usr/bin'],
-  unless  => "grep -q '^nofile 5000$' /etc/security/limits.conf",
+  unless  => "grep -q 'nofile 5000' /etc/security/limits.conf",
 }
