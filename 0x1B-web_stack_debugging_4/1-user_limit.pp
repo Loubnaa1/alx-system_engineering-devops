22cbcc7f80eb803d5dc6c51fbc8dc46d29cce02a
Loubnaa1 <loubnamerzak4@gmail.com>
2024-02-23 01:15:08 +0000

    proj

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,13 @@
 # login with holberton user without any error message
 
 exec { 'set-holberton-soft-nofile':
-  command => "sed -i '/^holberton soft nofile/c\holberton soft nofile 5000' /etc/security/limits.conf",
+  command => "sed -i 's/nofile/nofile 5000' /etc/security/limits.conf",
   path    => ['/bin', '/usr/bin'],
-  unless  => "grep -q '^holberton soft nofile 5000$' /etc/security/limits.conf",
+  unless  => "grep -q 'nofile 5000$' /etc/security/limits.conf",
 }
 
 exec { 'set-holberton-hard-nofile':
-  command => "sed -i '/^holberton hard nofile/c\holberton hard nofile 5000' /etc/security/limits.conf",
+  command => "sed -i 's/nofile/nofile 5000' /etc/security/limits.conf",
   path    => ['/bin', '/usr/bin'],
-  unless  => "grep -q '^holberton hard nofile 5000$' /etc/security/limits.conf",
+  unless  => "grep -q 'nofile 5000$' /etc/security/limits.conf",
 }
