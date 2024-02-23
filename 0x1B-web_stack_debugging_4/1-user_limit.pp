8481c926bdb3c053e4cbc9cb89ce1c5d784052bd
Loubnaa1 <loubnamerzak4@gmail.com>
2024-02-23 01:42:58 +0000

    proj

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,7 +2,7 @@
 
 
 exec { 'change_nofile_limit':
-  command => "sed -i '/^holberton soft nofile/s/.*/nofile 5000/' /etc/security/limits.conf",
+  command => "sed -i '/^nofile/s/.*/nofile 5000/' /etc/security/limits.conf",
   path    => ['/bin', '/usr/bin'],
   unless  => "grep -q '^nofile 5000$' /etc/security/limits.conf",
 }
